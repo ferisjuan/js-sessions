53ecddbcc049781179a1dad72b461f44fac94e86
Juan Feris <feris.juan@gmail.com>
2021-08-21 11:17:53 -0500

    add base url to .env, add axios

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hola mundo");
+import {config as dotenvConfig} from 'dotenv'
+dotenvConfig()
+
+import axios, {AxiosResponse} from "axios";
+
+import {baseUrl} from "./constants/url";
+
+axios.get(`${baseUrl}`)
+    .then((response: AxiosResponse<Record<string, string>>) => console.log(response.data['characteristic'])
+    )
+
+console.log(baseUrl);
